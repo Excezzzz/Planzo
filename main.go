6b599c185f9471426a255000d8d99885858a940d
excez <mihailmalcev375@gmail.com>
2025-05-24 15:59:59 +0300

    net/http

--- main.go
+++ main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
-    "net/http"
-    "github.com/labstack/echo/v4"
+	"encoding/json"
+	"net/http"
 )
 
+type task struct {
+	task   string
+	id     int
+	status bool
+}
+
+var tasks []task
+
 func main() {
-    e := echo.New()
+	http.HandleFunc("/task", taskHandler)
+	http.HandleFunc("/list", tasklistHandler)
+}
 
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Привет, Planzo!")
-    })
+func taskHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		gettask(w, r)
+	case http.MethodPost:
+		posttask(w, r)
+	default:
+		http.Error(w, "ivalid method", http.StatusMethodNotAllowed)
+	}
+}
 
-    e.Logger.Fatal(e.Start(":8080"))
+func gettask(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func posttask(w http.ResponseWriter, r *http.Request) {
+	var t task
+	err := json.NewDecoder(r.Body).Decode(&t)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tasks = append(tasks, t)
+	w.WriteHeader(http.StatusCreated)
 }

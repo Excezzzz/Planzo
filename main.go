e420047777eb1bc69ce12885d4d81a5fd9296928
excez <mihailmalcev375@gmail.com>
2025-05-24 16:50:15 +0300

    fix

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 )
 
 type task struct {
-	task   string
+	text   string
 	id     int
 	status bool
 }
@@ -16,6 +18,12 @@ var tasks []task
 func main() {
 	http.HandleFunc("/task", taskHandler)
 	http.HandleFunc("/list", tasklistHandler)
+
+	log.Println("server start")
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func taskHandler(w http.ResponseWriter, r *http.Request) {
@@ -31,16 +39,22 @@ func taskHandler(w http.ResponseWriter, r *http.Request) {
 
 func gettask(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(tasks)
+	fmt.Println("get tasks", tasks)
 }
 
 func posttask(w http.ResponseWriter, r *http.Request) {
-	var t task
-	err := json.NewDecoder(r.Body).Decode(&t)
+	var tasks task
+	err := json.NewDecoder(r.Body).Decode(&tasks)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	tasks = append(tasks, t)
+	tasks = append(tasks, task)
 	w.WriteHeader(http.StatusCreated)
+	fmt.Println("post new task", tasks)
+}
+
+func tasklistHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("web serner works correctly")
 }

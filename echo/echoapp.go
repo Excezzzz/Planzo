f63e82f1fedece91309083ff6f06e3ee63c115dc
excez <mihailmalcev375@gmail.com>
2025-05-26 01:49:45 +0300

    feature branch: added add and list commands

--- echo/echoapp.go
+++ echo/echoapp.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"sort"
 
 	"github.com/labstack/echo/v4"
 )
@@ -21,14 +23,34 @@ func main() {
 
 	e := echo.New()
 
-	e.GET("/addtask", func(c echo.Context) error {
-		addtaskHandler()
-		return c.String(http.StatusOK, "/addtask")
-	})
+	e.POST("/addtask", addtaskHandler)
+	e.GET("/list", listHandler)
 
 	e.Start(":8080")
 }
 
-func addtaskHandler() {
+func addtaskHandler(c echo.Context) error {
+	var t task
+	if err := c.Bind(&t); err != nil {
+		log.Println("Bad request")
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
 
+	t.ID = nextID
+	nextID++
+	t.ID = len(tasks) + 1
+
+	tasks = append(tasks, t)
+
+	sort.Slice(tasks, func(i, j int) bool {
+		return tasks[i].ID < tasks[j].ID
+	})
+
+	log.Println("Task created")
+	return c.String(http.StatusCreated, "Task created")
+}
+
+func listHandler(c echo.Context) error {
+	log.Println("Tasks list send")
+	return c.JSON(http.StatusOK, tasks)
 }

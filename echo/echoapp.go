d09a6573874b5b7a5b6bc8e3f818f89ea0b598da
excez <mihailmalcev375@gmail.com>
2025-05-26 15:24:29 +0300

    feature branch: full fuctional on echo

--- echo/echoapp.go
+++ echo/echoapp.go
@@ -25,6 +25,8 @@ func main() {
 
 	e.POST("/addtask", addtaskHandler)
 	e.GET("/list", listHandler)
+	e.PATCH("/status", statusHandler)
+	e.DELETE("/deletetask", deletetaskHandler)
 
 	e.Start(":8080")
 }
@@ -53,4 +55,69 @@ func addtaskHandler(c echo.Context) error {
 func listHandler(c echo.Context) error {
 	log.Println("Tasks list send")
 	return c.JSON(http.StatusOK, tasks)
+}
+
+func statusHandler(c echo.Context) error {
+	type update struct {
+		ID     int
+		Status bool
+	}
+
+	var upd update
+	if err := c.Bind(&upd); err != nil {
+		log.Println("Bad request")
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	found := false
+	for i, t := range tasks {
+		if t.ID == upd.ID {
+			tasks[i].Status = upd.Status
+			found = true
+			break
+		}
+	}
+
+	if !found {
+		log.Println("Task not found. Check ID")
+		return c.JSON(http.StatusNotFound, "Task not found. Check ID")
+	}
+
+	log.Println("Status updated")
+	return c.JSON(http.StatusOK, "Status updated")
+}
+
+func deletetaskHandler(c echo.Context) error {
+	type deletetask struct {
+		ID int
+	}
+
+	var del deletetask
+	if err := c.Bind(&del); err != nil {
+		log.Println("Bad request")
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	founddel := false
+	for i, t := range tasks {
+		if t.ID == del.ID {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			reorderIDs()
+			log.Println("Task deleted")
+			return c.JSON(http.StatusOK, "Task deleted")
+		}
+	}
+
+	if !founddel {
+		log.Println("Task not found. Check ID")
+		return c.JSON(http.StatusNotFound, "Task not found. Check ID")
+	}
+
+	return c.JSON(http.StatusOK, "Task deleted")
+}
+
+func reorderIDs() {
+	for i := range tasks {
+		tasks[i].ID = i + 1
+	}
 }
